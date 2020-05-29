2c006db8440941533213b8a29e2e28c461dc7e65
ChristopherEeles <christopher.eeles@outlook.com>
2020-05-28 22:55:35 -0400

    Update: One more function to finish for pathway analysis; 12.75/14

--- R/pathwayAnalysis.R
+++ R/pathwayAnalysis.R
@@ -1,3 +1,64 @@
+#'
+#' @param
+#'
+#'
+#' @export
+rankMetaClassGenesByEffSize <- function(metaEstimateStatsDT) {
+  meltDT <- melt(metaEstimateStatsDT, id.vars='gene', variable.name="class",
+                 value.name="wMean")
+  topGenes <- meltDT[order(-abs(wMean)), head(.SD, 1000), by=class]
+  return(topGenes)
+}
+
+#'
+#'
+#'
+#'
+#'
+loadGnSetGMTs <- function(gmtFldir) {
+  pathwayGMTfl <- list.files(gmtFldir, pattern='.gmt', full.names=TRUE)
+  pathwayL <- lapply(pathwayGMTfl, loadGSC)
+  pathwayNames <- lapply(pathwayL, function(GSC) gsub('_.*$', '', GSC$addInfo[1, 1]))
+  names(pathwayL) <- pathwayNames
+  return(pathwayL)
+}
+
+#'
+#'
+#'
+#'
+#'
+computePathwayScores <- function(rankedMetaClassGenes, pathwayL, referenceGenes) {
+  splitByClass <- split(rankedMetaClassGenes, by='class')
+
+  pathwayScoresDT <- list()
+  for (pathway in pathwayL) {
+    pathwayName <- gsub('_.*$', '', pathway$addInfo[1, 1])
+     pathwayScores <-
+        lapply(splitByClass,
+           function(DT, refGns)
+               runGSAhyper(genes=DT$gene, gsc=pathway, adjMethod="fdr", universe=refGns),
+           refGns=referenceGenes)
+    classScores <- lapply(pathwayScores,
+                          function(path) qnorm(1 - (path$pvalues/2)))
+
+    pathwayScoresDT[[pathwayName]] <- as.data.table(do.call(cbind, classScores))[, pathway := names(classScores[[1]])]
+    }
+
+
+}
+
+
+#'
+#'
+#'
+#'
+#'
+heatmapPathwayScores <- function() {
+
+}
+
+
 ########################################################################################################
 ### Required libraries
 #########################################################################################################
@@ -7,7 +68,7 @@ library(gplots)
 
 #########################################################################################
 load("../../data/meta_effectsize.RData")
-load('../../data/common_genes_cohorts_new.RData')       
+load('../../data/common_genes_cohorts_new.RData')
 
 
 metacluster1_genes=abs(mm$Metacluster1) >0.5
@@ -30,15 +91,15 @@ reference_genes= rownames(cohorts1$pcsi)
 hallmark= loadGSC("../../data/Pathway_files/h.all.v6.1.symbols.gmt")
 results_hallmark=runGSAhyper(rownames(metacluster1_genes),gsc= hallmark,  adjMethod="fdr",  universe= reference_genes)
 results_hallmark$resTab[which(results_hallmark$resTab[,2]<0.05),]
-clust1=qnorm(1 - (results_hallmark$pvalues/2)) 
+clust1=qnorm(1 - (results_hallmark$pvalues/2))
 
 results_hallmark=runGSAhyper(rownames(metacluster2_genes),gsc= hallmark,  adjMethod="fdr",  universe= reference_genes)
 results_hallmark$resTab[which(results_hallmark$resTab[,2]<0.05),]
-clust2=qnorm(1 - (results_hallmark$pvalues/2)) 
+clust2=qnorm(1 - (results_hallmark$pvalues/2))
 
 results_hallmark=runGSAhyper(rownames(metacluster3_genes),gsc= hallmark,  adjMethod="fdr",  universe= reference_genes)
 results_hallmark$resTab[which(results_hallmark$resTab[,1]<0.05),]
-clust3=qnorm(1 - (results_hallmark$pvalues/2)) 
+clust3=qnorm(1 - (results_hallmark$pvalues/2))
 
 zz=data.frame(clust1=clust1, clust2=clust2, clust3=clust3)
 zz[,1][is.infinite(zz[,1])] = NA
@@ -51,27 +112,27 @@ pdf("../results/Hallmark_genes_pathway.pdf")
 
 heatmap.2(data.matrix(zz),Rowv=TRUE, Colv=NA, col=colfunc(15), scale="column", margins=c(5,10), trace="none",dendrogram="row",cexRow=0.5,cexCol=1)
 
-####### Conanical 
+####### Conanical
 genesets=loadGSC("../../data/Pathway_files/c2.cp.v6.1.symbols.gmt", type="auto")
 
 results=runGSAhyper(rownames(metacluster1_genes),gsc= genesets,  adjMethod="fdr", universe= reference_genes)
 results$resTab[which(results$p.adj < 0.05),]
-clust1=qnorm(1 - (results$pvalues/2)) 
+clust1=qnorm(1 - (results$pvalues/2))
 pval1=results$pvalues
 
 results=runGSAhyper(rownames(metacluster2_genes),gsc= genesets,  adjMethod="fdr", universe= reference_genes)
 results$resTab[which(results$p.adj < 0.05),]
-clust2=qnorm(1 - (results$pvalues/2)) 
+clust2=qnorm(1 - (results$pvalues/2))
 pval2=results$pvalues
 
 results=runGSAhyper(rownames(metacluster3_genes),gsc= genesets,  adjMethod="fdr", universe= reference_genes)
 results$resTab[which(results$pvalues < 0.01),]
-clust3=qnorm(1 - (results$pvalues/2)) 
+clust3=qnorm(1 - (results$pvalues/2))
 pval3=results$pvalues
-# 
+#
 # results=runGSAhyper(rownames(metacluster4_genes),gsc= genesets,  adjMethod="fdr", universe= reference_genes)
 # results$resTab[which(results$p.adj < 0.05),]
-# clust4=qnorm(1 - (results$pvalues/2)) 
+# clust4=qnorm(1 - (results$pvalues/2))
 # pval4=results$pvalues
 
 
