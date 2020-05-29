b320111209c154b258f5650c39b14fc9b12a5d75
ChristopherEeles <christopher.eeles@outlook.com>
2020-05-29 16:27:58 -0400

    Update: Pathway Analyssi done; 13/14

--- R/clusterMetaEffectSizeCalculation.R
+++ R/clusterMetaEffectSizeCalculation.R
@@ -14,9 +14,9 @@ makeGenewiseCohortsDT <- function(preprocCohortDataL, annotSampMetaClassDT) {
   cohortNames <- mapply(rep, x=cohortNames, times=cohortLengths)
   mergeCohortDT <- rbindlist(cohDT)
   mergeCohortDT[, `:=`(cohorts=unlist(..cohortNames), samples=unlist(..cohortRNs))]
-  meltCohortDT <- melt(mergeCohortDT, id.vars=c('cohorts', 'samples'), 
+  meltCohortDT <- melt(mergeCohortDT, id.vars=c('cohorts', 'samples'),
                        value.name="expr", variable.name="gene")
-  genewiseCohortDT <- merge(meltCohortDT, annotSampMetaClassDT, 
+  genewiseCohortDT <- merge(meltCohortDT, annotSampMetaClassDT,
                             by=c('cohorts', 'samples'))
   return(genewiseCohortDT)
 }
@@ -31,42 +31,42 @@ makeGenewiseCohortsDT <- function(preprocCohortDataL, annotSampMetaClassDT) {
 calcClustMetaEstStats <- function(gwCohortsDT) {
   # Split into list of all cohort-gene combinations
   splitDT <- split(gwCohortsDT, by=c('cohorts', 'gene'))
-  
+
   message("Preprocessing the data...")
   for (DT in splitDT) {
     DT[, `:=`(respBasal=as.numeric(metaClasses == "Basal"),
               respClassical=as.numeric(metaClasses == "Classical"),
               respExocrine=as.numeric(metaClasses == "Exocrine"))]
   }
-  
+
   message("Calculating cohen.d estimates")
   # For each class, calculate the gene-wise cohen.d the predicted metaclass
   # (i.e., all samples for that gene and class)
   cohenEstimateL <- list()
   for (class in c("Basal", "Classical", "Exocrine")) {
-    cohenEstimateL[[class]] <- 
+    cohenEstimateL[[class]] <-
       lapply(splitDT, function(DT, class) {
         colName <- paste0("resp",  class)
         cohen.d(DT$expr, DT[[colName]], hedge.correct=TRUE)$estimate
       }, class=class)
   }
-  
+
   message("Calculating gene-wise standard deviation...")
-  # For each class, calculate the gene-wise standard deviation 
+  # For each class, calculate the gene-wise standard deviation
   # (i.e., all samples of for that gene and class)
   geneSD <- list()
   for (class in c("Basal", "Classical", "Exocrine")) {
-    geneSD[[class]] <- 
+    geneSD[[class]] <-
       lapply(splitDT, function(DT, class) {
         sd(DT[metaClasses == class, ]$expr)
       }, class=class)
   }
-  
+
   message("Reorganizing results...")
   # Flattent he lists
   cohL <- unlist(cohenEstimateL)
   wtL <- 1 / unlist(geneSD)
-  
+
   # Reform the lists grouped on class(i.e, L[[class]])
   cohLClass <- list()
   wtClass <- list()
@@ -74,8 +74,8 @@ calcClustMetaEstStats <- function(gwCohortsDT) {
     cohLClass[[class]] <- cohL[grepl(class, names(cohL))]
     wtClass[[class]] <- wtL[grepl(class, names(geneSD))]
   }
-  
-  # Reform the class lists grouped on gene (i.e, L[[class]][[gene]])
+
+  # Reform the class lists grouped on gene (i.e, L[[class]][[gene]][allCohortExpr])
   clGnCoh <- list()
   clGenWt <- list()
   for (class in c("Basal", "Classical", "Exocrine")) {
@@ -86,17 +86,18 @@ calcClustMetaEstStats <- function(gwCohortsDT) {
         wtClass[[class]][grepl(gene, names(wtClass[[class]]))]
     }
   }
-  
-  
+
+
   message("Computing weighted means...")
-  weighted.mean2 <- function (x, w, ..., na.rm = FALSE) 
+  # Add na.rm for
+  weighted.mean2 <- function (x, w, ..., na.rm = FALSE)
   {
     if (missing(w)) {
-      if (na.rm) 
+      if (na.rm)
         x <- x[!is.na(x)]
       return(sum(x)/length(x))
     }
-    if (length(w) != length(x)) 
+    if (length(w) != length(x))
       stop("'x' and 'w' must have the same length")
     if (na.rm) {
       i <- !is.na(x) & !is.na(w)
@@ -105,14 +106,14 @@ calcClustMetaEstStats <- function(gwCohortsDT) {
     }
     sum((x * w)[w != 0])/sum(w)
   }
-  
+
   weightedMean <- list()
   for (class in c("Basal", "Classical", "Exocrine")) {
-    weightedMean[[class]] <- mapply(weighted.mean2, 
-                                    x=clGnCoh[[class]], clGenWt[[class]], 
+    weightedMean[[class]] <- mapply(weighted.mean2,
+                                    x=clGnCoh[[class]], clGenWt[[class]],
                                     MoreArgs=list(na.rm=TRUE))
   }
-  
+
   weightedMeans <- as.data.table(do.call(cbind, weightedMean), keep.rownames="gene")
   return(weightedMeans)
 }
--- R/pathwayAnalysis.R
+++ R/pathwayAnalysis.R
@@ -31,21 +31,38 @@ loadGnSetGMTs <- function(gmtFldir) {
 computePathwayScores <- function(rankedMetaClassGenes, pathwayL, referenceGenes) {
   splitByClass <- split(rankedMetaClassGenes, by='class')
 
-  pathwayScoresDT <- list()
-  for (pathway in pathwayL) {
-    pathwayName <- gsub('_.*$', '', pathway$addInfo[1, 1])
+  pathStats <- list()
+  pvalsDT <- list()
+  for (path in pathwayL) {
+    pathwayName <- gsub('_.*$', '', path$addInfo[1, 1])
      pathwayScores <-
         lapply(splitByClass,
-           function(DT, refGns)
-               runGSAhyper(genes=DT$gene, gsc=pathway, adjMethod="fdr", universe=refGns),
-           refGns=referenceGenes)
+           function(DT, refGns, path)
+               runGSAhyper(genes=DT$gene, gsc=path, adjMethod="fdr", universe=refGns),
+           refGns=referenceGenes,
+           path=path)
+    pvals <- lapply(pathwayScores, `[[`, "pvalues")
     classScores <- lapply(pathwayScores,
                           function(path) qnorm(1 - (path$pvalues/2)))
 
-    pathwayScoresDT[[pathwayName]] <- as.data.table(do.call(cbind, classScores))[, pathway := names(classScores[[1]])]
-    }
+    pathwayScoresDT <-
+        as.data.table(do.call(cbind, classScores))[, pathway := names(classScores[[1]])]
+    pvalsDT <-
+        as.data.table(do.call(cbind, pvals))[, pathway := names(classScores[[1]])]
 
+    pathScoreDT <-  melt(pathwayScoresDT, id.vars="pathway",
+                          value.name="pathScore", variable.name="class",
+                          variable.factor=FALSE
+                          )
+    pDT <- melt(pvalsDT, id.vars="pathway",
+                      value.name="pval", variable.name="class",
+                      variable.factor=FALSE)
 
+    pathwayStats <- merge(pathScoreDT, pDT, by=c("class", "pathway"))
+    pathStats[[pathwayName]] <- pathwayStats[, geneSet := rep(pathwayName, nrow(pathwayStats))]
+  }
+  pathStatsDT <- rbindlist(pathStats)[is.infinite(pathScore), pathScore := NA]
+  return(pathStatsDT)
 }
 
 
@@ -54,8 +71,38 @@ computePathwayScores <- function(rankedMetaClassGenes, pathwayL, referenceGenes)
 #'
 #'
 #'
-heatmapPathwayScores <- function() {
+heatmapPathwayScores <- function(pathStatsDT, exclude, significance=0.05) {
 
+    if (!missing(exclude)) {
+        pStatsDT <- pathStatsDT[pval < significance & !(class %in% exclude), ][, -'pval']
+
+    } else {
+        pStatsDT <- pathStatsDT[pval < significance, ][, -'pval']
+    }
+
+    pStatsDT <- dcast(pStatsDT, geneSet + pathway ~ class , value.var='pathScore')
+    pStatsDT <- na.omit(pStatsDT)
+
+    splitDT <- split(pStatsDT, by="geneSet")
+
+    preprocMats <- lapply(splitDT, function(DT) {
+        mat <- as.matrix(DT[, .SD, .SDcols=-c("geneSet", "pathway")])
+        rownames(mat) <- DT$pathway
+        mat
+    })
+
+    colorFun <- colorRampPalette(c("gold", "white", "black"))
+    plotExpressions <- lapply(preprocMats,
+                    function(mat)
+                        as.expression(call('heatmap.2', x=mat, Rowv=TRUE, Colv=NA,
+                                          col=colorFun(15), lhei=c(0.05, 0.20),
+                                          scale="column", margins=c(5, 10),
+                                          trace="none", dendrogram="row",
+                                          cexRow=0.5, cexCol=1))
+                    )
+
+    plots <- lapply(plotExpressions, as.ggplot)
+    return(plots)
 }
 
 
@@ -71,9 +118,9 @@ load("../../data/meta_effectsize.RData")
 load('../../data/common_genes_cohorts_new.RData')
 
 
-metacluster1_genes=abs(mm$Metacluster1) >0.5
-metacluster2_genes=abs(mm$Metacluster2) >0.5
-metacluster3_genes=abs(mm$Metacluster3) >0.5
+metacluster1_genes=abs(mm$Metacluster1) > 0.5
+metacluster2_genes=abs(mm$Metacluster2) > 0.5
+metacluster3_genes=abs(mm$Metacluster3) > 0.5
 
 
 c3 <- cbind(metacluster1_genes, metacluster2_genes, metacluster3_genes)
