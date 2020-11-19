d18e6a2fd790917e0f2a9bbe2621b3aaca677eaf
ChristopherEeles <christopher.eeles@outlook.com>
2020-11-19 17:26:54 -0500

    Add: invert argument to subset CohortList, lets you invert matches

--- R/methods-subset.R
+++ R/methods-subset.R
@@ -3,8 +3,10 @@
 #' Works using endoapply of `[` over the list `SurvivalExperiment`s
 #'
 #' @param x A `CohortList` object
-#' @param i The row query. Defaults to TRUE, i.e., select all.
-#' @param j The column query. Defaults to TRUE, i.e., select all.
+#' @param subset The row query. Defaults to TRUE, i.e., select all.
+#' @param select The column query. Defaults to TRUE, i.e., select all.
+#' @param invert A `logical` vector indicating if the matches should be
+#'   inverted. Default is FALSE.
 #'
 #' @return A `CohortList` containing only the rows and columns selected in i
 #'   and j, respectively.
@@ -13,7 +15,15 @@
 #' @md
 #' @export
 setMethod('subset', signature(x='CohortList'),
-    function(x, subset=TRUE, select=TRUE)
+    function(x, subset=TRUE, select=TRUE, invert=FALSE)
 {
+    if (invert) {
+        rows <- lapply(x, rownames)
+        keepRows <- lapply(rows, setdiff, y=subset)
+        cols <- lapply(x, colnames)
+        keepCols <- lapply(cols, setdiff, y=select)
+        return(mendoapply(`[`, x=x, i=keepRows, j=keepCols))
+    }
+
     return(endoapply(x, `[`, i=subset, j=select))
 })
