37cd81d7cc7d6e64a9529a45776bf029638191c1
ChristopherEeles <christopher.eeles@outlook.com>
2020-11-22 16:41:34 -0500

    Update: Now merging all assays for training a KTSP model into a single SurvivalExperiment; this ensures they use the same survival data as well as that they share genes and samples; PCOSP class now extends SurvivalExperiment adding additional metadata

--- R/class-PCOSP.R
+++ R/class-PCOSP.R
@@ -7,7 +7,7 @@
 #'   developer use.
 #'
 #' @md
-#' @include options.R
+#' @include class-SurvivalExperiment.R
 #' @export
 ## TODO:: Can I subclass a BiocConductor object for this?
 setClass("PCOSP", contains='SurvivalExperiment',
@@ -27,92 +27,33 @@ setClass("PCOSP", contains='SurvivalExperiment',
 #'
 #' @md
 #' @export
-PCOSP <- function(trainCohorts) {
+PCOSP <- function(trainCohort) {
 
-    if (!is(trainCohorts, 'CohortList'))
+    if (!is(trainCohorts, 'SurvivalExperiment'))
         stop(.errorMsg(.context(), 'The trainCohorts argument is not a ',
-            'CohortList object. Please convert it before building a PCOSP ',
-            'model!'))
+            'SurvivalExperiment object. Please convert it before building a ',
+            'PCOSP model!'))
 
-    ## TODO:: Ensure the SurvivalExperiments have different mDataTypes?
-    if (length(unique(mcols(trainCohorts)$mDataType)) <
-        nrow(mcols(trainCohorts)))
-    {
-        stop(.errorMsg(.context(), 'The training cohorts have the same ',
-            'molecular data type. Please manually merge all cohorts with the ',
-            'same data type using some summary statistic such as mean, median, ',
-            'etc., before passing into a PCOSP model.'))
+    assaysL <- assays(trainCohort)
+    for (i in seq_along(assaysL)) {
+        rownames(assaysL[[i]]) <- paste0(names(assaysL)[i], rownames(assaysL),
+            sep='.')
     }
+    modelMatrix <- do.call(rbind, assaysL)
 
-    # ensure the SurivalExperiments have common genes
-    commonGenes <- findCommonGenes(trainCohorts)
-    actualGenes <- Reduce(intersect, lapply(trainCohorts, rownames))
-
-    # subset to common genes
-    if (!all(actualGenes %in% commonGenes)) {
-        trainCohorts <- subset(trainCohorts, subset=commonGenes)
-        warning(.warnMsg(.context(), 'The training cohorts did not have only ',
-            'common genes. Subsetting to common genes...'))
-    }
-
-    # ensure the SurvivalExperiments have common samples
-    commonSamples <- findCommonSamples(trainCohorts)
-    actualSamples <- Reduce(intersect, lapply(trainCohorts, colnames))
-
-    # subset to common samples
-    if (!all(actualSamples %in% commonSamples)) {
-        trainCohorts <- subset(trainCohorts, select=commonSamples)
-        warning(.warnMsg(.context(), 'The training cohorts did not have only ',
-                         'common samples Subsetting to common samples...'))
-    }
-
-    # concatenate the datatypes to label genes
-    cohortAssays <- lapply(trainCohorts, assay, 1)
-    for (i in seq_along(cohortAssays)) {
-        rownames(cohortAssays[[i]]) <-
-            paste(mcols(trainCohorts)$mDataType[i],
-                rownames(cohortAssays[[i]]), sep='.')
-    }
-    modelMatrix <- do.call(rbind, cohortAssays)
-
-    .getSurvivalData <-
-        function(x) c(colData(x)$days_survived, colData(x)$is_deceased)
-
-    survivalData <- lapply(trainCohorts, .getSurvivalData)
-
-    if (!all(survivalData[[1]] == survivalData[[2]]))
-        stop(.errorMsg(.context(), 'The training cohorts for a PCOSP model ',
-            'should be on the same samples, but the survival data in the ',
-            'colData slot of the SurvivalExperiments are different. Please ',
-            'ensure the molecular data in each survival experiment is the same!'))
-
-    # get the survived data
-    survivalData <- data.table(
-        sample=
-    )
-
-    new('PCOSP', trainCohorts=trainCohorts, modelMatrix=modelMatrix)
+    new('PCOSP', colData=colData(trainCohort),
+        rowData(rowData(trainCohort)), assays=modelMatrix,
+        metadata=metadata(trainCohort))
 }
 
-#'
-#' @importFrom settings is_setting clone_and_merge
-#' @export
-setMethod('manageOptions', 'PCOSP', function(where=NULL, ...) {
-  if (settings::is_setting(...)) {
-    where@options <- settings::clone_and_merge(where@options(), ...)
-    where
-  } else {
-    where@options(...)
-  }
-})
-
-## TODO:: Validity method?
-
-#' #'
-#' #'
-#' #'
-#' #'
-#' #'
-#' setValidity('PCOSP', function(object) {
-#'
-#' })
+##'
+##' @importFrom settings is_setting clone_and_merge
+##' @export
+#setMethod('manageOptions', 'PCOSP', function(where=NULL, ...) {
+#  if (settings::is_setting(...)) {
+#    where@options <- settings::clone_and_merge(where@options(), ...)
+#    where
+#  } else {
+#    where@options(...)
+#  }
+#})
