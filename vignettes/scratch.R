b516a6587396713e55642046e4498e738ab0bff7
ChristopherEeles <christopher.eeles@outlook.com>
2021-02-02 18:25:01 -0500

    Add: model predictions, external validation and validation visualization for PCOSP model object

--- vignettes/scratch.R
+++ vignettes/scratch.R
@@ -1,1 +1,4 @@
-trainedPCOSPmodel <- trainModel(PCOSPmodel, numModels=10, minAccurary=0.6)
+#trainedPCOSPmodel <- trainModel(PCOSPmodel, numModels=10, minAccurary=0.6)
+
+validatedPCOSPmodel <- validateModel(trainedPCOSPmodel,
+    valData=PCOSPpredValCohorts)
