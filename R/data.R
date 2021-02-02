453927c5d1f2f92453ca312bf0d1283ab4b76ca6
ChristopherEeles <christopher.eeles@outlook.com>
2021-02-01 21:39:31 -0500

    Add: Vignette file with PCOSP example

---
+++ R/data.R
@@ -0,0 +1,17 @@
+#' sampleCohortList
+#'
+#' A `CohortList` object containing sample data for the PCOSP vignette.
+#'
+#' @seealso `MetaGxPancreas::loadPancraseDatasets`
+#'
+#' @md
+#' @export
+NULL
+
+#' sampleCohortClasses
+#'
+#' A `list` of `data.frames` containing clinical subtypes for the data
+#'   in the `sampleCohortList` object.
+#'
+#' @export
+NULL
