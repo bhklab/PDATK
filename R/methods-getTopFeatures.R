e06d885273a5a36e50f55114ef3dd0d9537acd5a
ChristopherEeles <christopher.eeles@outlook.com>
2021-02-24 19:15:59 -0500

    Add: getTopFeatures method for SummarizedExperiment which uses rankFeatures if ranks are missing

--- R/methods-getTopFeatures.R
+++ R/methods-getTopFeatures.R
@@ -42,3 +42,48 @@ setMethod('getTopFeatures', signature(object='PCOSP'),
     topFeatures <- unique(unlist(TSPs))
     return(topFeatures)
 })
+
+#' Get the Top Ranked Features in a `SummarizedExperiment` object
+#'
+#' @param object A `SummarizedExperiment` to extract top features from
+#' @param numFeats An `integer` number of top ranked features to extract.
+#' @param ... Fall through arguments to `rankFeatures`, which is used to
+#'   calculate the ranks if `rankCol` is not present the object `rowData`.
+#' @param rankCol The name of the column containing the integer feature ranks.
+#'   Defaults to `feature_rank`, as calculated with `rankFeatures`, but users
+#'   can alternatively specify their own custom rank column if desired.
+#'
+#' @return A `character` vector of top ranked features, with the features in
+#'   order of rank ascending.
+#'
+#' @examples
+#' data(sampleICGCmicro)
+#' getTopFeatures(sampleICGCmicro)
+#'
+#' @md
+#' @export
+setMethod('getTopFeatures', signature(object='SummarizedExperiment'),
+    function(object, numFeats, ..., rankCol='feature_rank')
+{
+    funContext <- .context(1)
+    if (!(rankCol %in% colnames(rowData(object)))) {
+        if (rankCol != ' feature_rank') {
+            warning(.warnMsg(funContext, 'The ',
+                'column ', rankCol, ' is missing from the data. Calculating new ',
+                'ranks with `rankFeature` and using the "feature_rank" column ',
+                'to get the top features'))
+            rankCol <- 'feature_rank'
+        }
+        object <- rankFeatures(object, ...)
+    }
+    if (!is.numeric(numFeats)) stop(.errorMsg(funContext, 'The numFeats ',
+        'parameter is not numeric!'))
+
+    sortedRowData <- rowData(object)[order(rowData(object)[[rankCol]]), ]
+    if (is.null(rownames(sortedRowData))) stop(.errorMsg('This function ',
+        'requires rownames to work, please assign your feature names to the ',
+        'rownames of rowData!'))
+    topFeatures <- rownames(sortedRowData)[seq_len(numFeats)]
+
+    return(topFeatures)
+})
