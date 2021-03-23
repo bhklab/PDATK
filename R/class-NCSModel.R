491f3736b0cfedb86abbe8dd0aaef5041112c6d8
ChristopherEeles <christopher.eeles@outlook.com>
2021-03-23 19:23:26 -0400

    Add: NetworkCommunitySearchModel to modularize metacluster prediction

--- R/class-NCSModel.R
+++ R/class-NCSModel.R
@@ -1,29 +1,25 @@
-#' Generic for Predicting Metaclusters Based on A Trained S4Model
+#' S4Model Class for Metaclustering Using Network Community Search
 #' 
-#' @param object An `S4` object to predict classes for.
-#' @param model An `S4` object representing a classification or regression model.
-#'   Should inherit from `S4Model`.
-#' 
-#' @return The `object` argument with metacluster predictions in the column
-#'   metadata.
+#' @inherit S4Model
 #' 
 #' @md
+#' @include class-S4Model.R
 #' @export
-setGeneric('predictMetaclasses', function(object, model, ...) 
-    standardGeneric('predictMetaclasses'))
+.NCSModel <- setClass('NCSModel',
+    contains='S4Model'
+)
 
-#' predictMetaclasses Method for ConsensusMetaclusteringModel
+#' Constructor for a NetworkCommunitySearchModel (NCSModel)
 #' 
 #' Use `igraph::fastgreedy.community` cross-cohort metaclusters for
 #'   a `ConsensusMetaclusteringModel` which has been validated with 
 #'   `validateModel`.
 #' 
-#' @param object Not used, should be omitted.
 #' @param model A validated `ConsensusMetaclusteringModel` object.
 #' @param alpha A `float` specifying the significance level for cluster
 #'   reproducibility. Default is 0.05.
-#' @param minRepro A `float` specifying the minimum in-group proportion for
-#'   a cluster to be included in the metacluster labels. Default is 0.5.
+#' @param minRepro A `float` specifying the minimum in-group proportion (IGP) 
+#'   for a cluster to be included in the metacluster labels. Default is 0.5.
 #' @param minCor A `float` specifying the minimum correlation between a 
 #'   centroid and assay cluster to be included in the metacluster labels. 
 #'   Default is 0.0.
@@ -33,13 +29,13 @@ setGeneric('predictMetaclasses', function(object, model, ...)
 #' @importFrom igraph graph_from_edgelist layout_with_fr as.undirected
 #'     fastgreedy.community
 #' @importFrom data.table data.table as.data.table merge.data.table rbindlist
-#'   `:=` copy .N .SD fifelse merge.data.table transpose setcolorder
+#'   `:=` copy .N .SD fifelse merge.data.table transpose setcolorder setnames
+#' @aliases NCSModel
 #' @export
-setMethod('predictMetaclasses', signature(object='missing', 
-    model='ConsensusMetaclusteringModel'), function(object, model, alpha=0.05, 
-        minRepro=0.5, minCor=0.0) 
+NetworkCommunitySearchModel <- function(model, alpha=0.05, minRepro=0.5, 
+    minCor=0.0) 
 {
-    funContext <- .context()
+    funContext <- .context(1)
 
     cohortMAE <- c(trainData(model), validationData(model)$experiments)
     corTestStats <- validationStats(model)[metric == 'cor.test', ]
@@ -56,7 +52,7 @@ setMethod('predictMetaclasses', signature(object='missing',
             'check the validationStats of your ', class(model)[1] ,' object.'))
     }
 
-    # Prepare corrleation threshold DT for join
+    # Prepare correlation threshold DT for join
     setnames(corThresholdDT, 'estimate', 'cor_threshold', skip_absent=TRUE)
     corThresholdDT[, c('metric', 'p_value', 'assay_N') := NULL]
     
@@ -69,25 +65,19 @@ setMethod('predictMetaclasses', signature(object='missing',
     clusterEdgeDT <- merge.data.table(corThresholdDT, clusterReproStats, 
         by=intersect(colnames(corThresholdDT), colnames(clusterReproStats)))
 
-    # Filter to signficant cluster edges based on input parameters
-    signifClusterEdgeDT <- clusterEdgeDT[
-        p_value <= alpha &
-        ingroup_proportion >= minRepro &
-        cor_threshold >= minCor,
-    ]
+    .NCSModel(
+        trainData=cohortMAE,
+        modelParams=SimpleList(list(
+            alpha=alpha,
+            minRepro=minRepro,
+            minCor=minCor
+        )
+        ),
+        models=SimpleList(list(
+            networkEdges=clusterEdges
+        ))
+    )
 
-    # Extract the edge labels for network community search
-    clusterEdges <- as.matrix(signifClusterEdgeDT[, 
-        .(centroid_cluster=paste0(centroid_cohort, '-', centroid_K), 
-            assay_cluster=paste0(assay_cohort, '-', assay_K))
-    ])
-
-    # Construct network graph and identify metaclusters using network community
-    #   search
-    graph <- graph_from_edgelist(clusterEdges)
-    coords <- layout_with_fr(graph)
-    ugraph <- as.undirected(graph)
-    metaClusters <- fastgreedy.community(ugraph, 
-        weights=signifClusterEdgeDT$cor_threshold)
-
-})
+}
+#' @export
+NCSModel <- NetworkCommunitySearchModel
--- R/class-S4Model.R
+++ R/class-S4Model.R
@@ -17,7 +17,7 @@ setClassUnion('DFrame_or_data.frame_data.table_or_NULL',
 #'   the data used to validate or evaluate the performance of a model.
 #' @slot mcols A `DataFrame` or 'data.frame' of item metadata for the `models` 
 #'   slot.
-#' @slot metadaata A `List` or `list` of model level metadata.
+#' @slot metadata A `List` or `list` of model level metadata.
 #' 
 #' @md
 #' @keywords internal
